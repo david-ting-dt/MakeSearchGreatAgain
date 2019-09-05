816855e07d57b008fc0270ed5087007922502527
David Ting <davidting3250@gmail.com>
2019-09-06 09:24:24 +1200

    updated scripts.js

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,7 @@ function App() {
     // });
 
     const onClickHandler = () => {
-        console.log("onclick")
+        
 
     }
 
--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,34 +1,7 @@
-const elasticsearch = require('elasticsearch');
+export const httpGet = (searchTerm) => {
+    const URL = `https://search-clients-hackdays2019-2-l5jsbxllcgxsaxostzcs2uc2im.ap-southeast-2.es.amazonaws.com/clients/_search?q=*${searchTerm ?searchTerm : ':'}*`
 
-export const httpGet = () => {
-
-    const client = new elasticsearch.Client({
-        host: '<a class="vglnk" href="http://username" rel="nofollow"><span>http</span><span>://</span><span>username</span></a>:password@host:port/'
-        // <a class="vglnk" href="http://localhost:9200/" rel="nofollow"><span>http</span><span>://</span><span>localhost</span><span>:</span><span>9200</span><span>/</span></a> 
-        // <a class="vglnk" href="http://root" rel="nofollow"><span>http</span><span>://</span><span>root</span></a>:12345@localhost:9200/ 
-        // If you have set username and password
-    });
-
-    // Check if Connection is ok or not
-    client.ping({
-        // ping usually has a 3000ms timeout
-        requestTimeout: Infinity,
-    }, function (error) {
-        if (error) {
-            console.trace('elasticsearch cluster is down!');
-        } else {
-            console.log('All is well');
-        }
-    });
-
-    client.search({
-        index: "crud", // Your index name for example crud
-        type: "doc", // Your index name for example doc    
-    }).then(function (resp) {
-        return resp.hits.hits;
-    }, function (err) {
-        console.log(err.message);
-    });
+    fetch(URL).then(resp => console.log(resp));
 }
 
 
