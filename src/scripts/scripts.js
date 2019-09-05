8ff7dbd92e78d1968b8db66f3c697fa1a48138dc
David Ting <davidting3250@gmail.com>
2019-09-05 12:40:47 +1200

    updated script

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,3 +1,11 @@
 export const httpGet = () => {
-    console.log('a');
+    const URL = "http://api.icndb.com/jokes";
+
+    fetch(URL, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+    .then(response => response.json()).then(response => console.log(response));
 }
