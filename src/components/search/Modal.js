164c7e645e4deb32030d4c7ad599fd07e22e32e9
onizsmx <onizsmx@gmail.com>
2019-09-06 13:29:52 +1200

    id filter commented out

--- src/components/search/Modal.js
+++ src/components/search/Modal.js
@@ -2,18 +2,18 @@ import React, { Fragment } from 'react';
 
 const Modal = props => {
     const details = Object.keys(props.data).map((key, index) => {
-        if (key !== 'id') {
-            return (
-                <div key={index} className='info-line d-flex'>
-                    <div className='info-key'>
-                        {key}
-                    </div>
-                    <div className='info-value'>
-                        {props.data[key]}
-                    </div>
+        // if (key !== 'id') {
+        return (
+            <div key={index} className='info-line d-flex'>
+                <div className='info-key'>
+                    {key}
                 </div>
-            );
-        }
+                <div className='info-value'>
+                    {props.data[key]}
+                </div>
+            </div>
+        );
+        // }
     });
 
     return (
