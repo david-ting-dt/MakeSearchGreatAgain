d6f32c34b467607e233fc50fa6c8d2d64f3c20ed
onizsmx <onizsmx@gmail.com>
2019-09-06 12:38:35 +1200

    types added

--- src/components/search/DisplayArea.js
+++ src/components/search/DisplayArea.js
@@ -1,15 +1,16 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 
-const DisplayArea = ({data}) => {
+const DisplayArea = ({ data }) => {
     console.log(data)
     const contacts = data && data.map(client => {
         const { _id: id, _source: source } = client;
-        const { firstname, lastname } = source;
+        const { firstname, lastname, contactType } = source;
         return (
             <Fragment key={id}>
                 <div className="row display-item">
-                    <div className="col-md-6 row-content">{firstname}</div>
-                    <div className='col-md-6 row-content'>{lastname}</div>
+                    <div className='col-md-4 row-content'>{contactType}</div>
+                    <div className="col-md-4 row-content">{firstname}</div>
+                    <div className='col-md-4 row-content'>{lastname}</div>
                 </div>
                 <div className='content-divider' />
             </Fragment>
@@ -19,10 +20,13 @@ const DisplayArea = ({data}) => {
     return (
         <div className='display-area'>
             <div className='row display-area-header'>
-                <div className='col-md-6 row-content'>
+                <div className='col-md-4 row-content'>
+                    Type
+                </div>
+                <div className='col-md-4 row-content'>
                     First name
                 </div>
-                <div className='col-md-6 row-content'>
+                <div className='col-md-4 row-content'>
                     Last name
                 </div>
             </div>
